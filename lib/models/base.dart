2213cb6781011d7c105a9ac7e1ab98f8c4540698
Damola Adekoya <adekoyafelix@gmail.com>
2020-01-24 15:12:48 +0100

    ReadME updated

--- lib/models/base.dart
+++
@@ -1,6 +0,0 @@
-class BasePlaceholder {
-  BasePlaceholder() {}
-  BasePlaceholder fromJson(Map<String, dynamic> json) {
-    return BasePlaceholder();
-  }
-}
--- test/jsonplaceholder_offline_test.dart
+++ test/jsonplaceholder_offline_test.dart
@@ -7,18 +7,30 @@ import 'package:jsonplaceholder_offline/models/todo.dart';
 import 'package:jsonplaceholder_offline/models/user.dart';
 
 void main() {
-  test('adds one to input values', () {
-    JsonPlaceholder jsonplaceholder = JsonPlaceholder();
-    var users = jsonplaceholder.getData<Todo>(length: 1);
-    print(users);
-  });
+  // test('adds one to input values', () {
+  //   JsonPlaceholder jsonplaceholder = JsonPlaceholder();
+  //   var users = jsonplaceholder.getData<Todo>(length: 1);
+  //   print(users);
+  // });
 
   group('testing Jsonplaceholder Offline package', () {
-    test('it should return an object', () {
-      JsonPlaceholder jsonplaceholder = JsonPlaceholder();
-      User user = jsonplaceholder.getData<User>(length: 1);
-      expect(user, !null);
+    // test('it should return an object', () {
+    //   JsonPlaceholder jsonplaceholder = JsonPlaceholder();
+    //   User user = jsonplaceholder.getData<User>(length: 1);
+    //   expect(user, !null);
+    // });
+
+    test('it should have length > 1', () {
+      JsonPlaceholder jsonPlaceholder = JsonPlaceholder();
+      List<User> users = jsonPlaceholder.getData<User>(length: 20);
+      expect(users.length, 20);
     });
+
+    // test('it should throw an Exception', () {
+    //   JsonPlaceholder jsonPlaceholder = JsonPlaceholder();
+    //   User user = jsonPlaceholder.getData(length: 2);
+    //   expect(user, throw ('Invalid object type'));
+    // });
   });
 }
 
